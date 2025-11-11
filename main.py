eef829594f073def041e58ffa15b076dcd6b38c3
Anubhav Saini <anubhavsaini2506@gmail.com>
2025-11-11 14:22:42 +0000

    Added the new entry function in the personal diary

--- main.py
+++ main.py
@@ -16,3 +16,17 @@ while True:
         print("Invalid input! Please enter a number 1-5.")
         continue
     
+
+    match(x):
+        case 1:
+             print("Let's write a new entry")
+             date=input("Enter date in the format ddmmyyyy ")
+             if len(date) !=8 or not date.isdigit() or date.endswith('.txt'):
+                print("Enter date in correct format \n")
+                continue
+             filename = os.path.join(diary_folder,date +'.txt')
+             with open(filename,'w') as f:
+                text=input("Enter the title for the day ")
+                f.write("Title: " + text+"\n Date: " + date + "\n --------------------- \n")
+                string=input("Enter what happened all the day \n")
+                f.write(string)
